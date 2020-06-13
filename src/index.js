1942c7c97b9f72205718fead4023cbd14a5effe8
Cristian Bello Cervantes <cristiancervantes.cbc@gmail.com>
2020-06-12 20:28:06 -0500

    Add challenge solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
-
+  const numbers = []
+  for(let i = 0; i <= n ; i++){
+    if(i > 1){
+      let num = numbers[i - 2] + numbers[i - 1]
+      numbers.push(num)
+    }else{
+      numbers.push(i)
+    }
   }
-
-
-  module.exports = fibonacci;
+  numbers.shift()
+  return numbers
+}
+module.exports = fibonacci;
